7318964f1ce1255af4617f035bbb1d3c2d6248ed
Shelby Clayton and Stephen Emery <shelby+stephen@>
2016-12-07 16:01:12 -0800

    Add createAccountActivity.

--- app/src/main/java/com/example/guest/mytestproject/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/mytestproject/ui/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.guest.mytestproject.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.guest.mytestproject.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
